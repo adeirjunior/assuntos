34cb477da9914d22f7391b16c13895be3eeef173
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2023-01-05 21:26:57 -0400

    mudado  estilo das tags code

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,9 @@
 
 export default function Footer() {
   return (
-    <footer className="bg-dark text-light flex flex-col justify-between p-6 text-sm mt-16 w-auto h-80">
-      <p className="selection:bg-light selection:text-dark">Feito por <a target='github' className="underline hover:opacity-70" href="https://github.com/adeirjunior">Adeir Junior</a></p>
-      <p className="selection:bg-light selection:text-dark text-right">Ele aceitaria um <a target='kofi' className="underline hover:opacity-70" href="https://ko-fi.com/adeirjunior">Café</a></p>
+    <footer className="bg-dark flex flex-col justify-between p-6 text-sm mt-16 w-auto h-80">
+      <p className="text-light selection:bg-light selection:text-dark">Feito por <a target='github' className="underline hover:opacity-70" href="https://github.com/adeirjunior">Adeir Junior</a></p>
+      <p className="text-light selection:bg-light selection:text-dark text-right">Ele aceitaria um <a target='kofi' className="underline hover:opacity-70" href="https://ko-fi.com/adeirjunior">Café</a></p>
     </footer>
   )
 }
