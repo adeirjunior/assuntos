cd3cedf422556feda81741b36c9d11ca0c904707
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2023-03-08 00:07:29 -0400

    footer link changed from github to portfolio site

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@
 export default function Footer() {
   return (
     <footer className="bg-dark flex flex-col justify-between p-6 text-sm mt-16 w-auto h-80">
-      <p className="text-light selection:bg-light selection:text-dark">Feito por <a target='github' className="underline hover:opacity-70" href="https://github.com/adeirjunior">Adeir Junior</a></p>
+      <p className="text-light selection:bg-light selection:text-dark">Feito por <a target='adeir' className="underline hover:opacity-70" href="https://adeir.me">Adeir Junior</a></p>
       <p className="text-light selection:bg-light selection:text-dark text-right">Ele aceitaria um <a target='kofi' className="underline hover:opacity-70" href="https://ko-fi.com/adeirjunior">Café</a></p>
     </footer>
   )
